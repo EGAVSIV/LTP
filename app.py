06e5828dbd04866aa1aa163ab2be252f60ddd86f
Gaurav Singh Yadav <137716439+EGAVSIV@users.noreply.github.com>
2026-01-14 20:18:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,16 @@ from symbols import NSE_INDICES, NSE_STOCKS, COMMODITIES
 st.set_page_config(layout="wide")
 st.title("📊 Live NSE & Commodity Dashboard")
 
+def check_breakout(curr, prev):
+    if curr is None or prev is None:
+        return None
+    if curr > prev:
+        return "UP"
+    elif curr < prev:
+        return "DOWN"
+    return None
+
+
 # ==================== SOUND ====================
 ALERT_SOUND = "assets/alert.mp3"
 
